d4e76663cdb097249179f5af66ac1a93f3710dd7
Jije <guillez3198@gmail.com>
2022-09-24 20:51:30 +0200

    Auto stash before merge of "BackStage" and "origin/BackStage"

--- Indie Spain Jam/Assets/Scripts/Manager/txtAssistant.cs
+++ Indie Spain Jam/Assets/Scripts/Manager/txtAssistant.cs
@@ -9,10 +9,23 @@ public class txtAssistant : MonoBehaviour
     [SerializeField] private TxtManager txtManager;
     [SerializeField] private string[] writeTxt;
     [SerializeField] TextMeshProUGUI txt;
+    private TextMeshProUGUI TextoMejoras;
 
+    private void Awake()
+    {
+        TextoMejoras = FindObjectOfType<TextMeshProUGUI>();
+    }
 
     private void Start()
     {
-       // txtManager.AddWriter(txt, writeTxt[0], 0.1f);
+        //txtManager.AddWriter(TextoMejoras, writeTxt[0], 0.1f);
+        //TextoMejoras.SetText("Hola Caracola");
+        //TextoMejoras.text = "Arriba chuta la victoria es tuya";
+    }
+
+    void MejoraMedicina()
+    {
+        
+        txtManager.AddWriter(TextoMejoras, writeTxt[0], 0.1f);
     }
 }
