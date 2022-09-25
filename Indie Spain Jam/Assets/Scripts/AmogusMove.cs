d010b3013125521a24c494ea067846a735ae3797
TermiSenpai <escarpaprieto@gmail.com>
2022-09-25 20:57:24 +0200

    Amogus

---
+++ Indie Spain Jam/Assets/Scripts/AmogusMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmogusMove : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * rotationSpeed);
+    }
+}
